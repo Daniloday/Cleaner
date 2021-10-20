19b385329e50c3d056e5d5bc1773836f2e9239b4
Nikita <prilki.neket@gmail.com>
2021-10-20 20:06:57 +0300

    todo and guideline

--- app/src/main/java/com/missclickads/cleaner/ui/batteryoptimizer/BatteryOptimizerFragment.kt
+++ app/src/main/java/com/missclickads/cleaner/ui/batteryoptimizer/BatteryOptimizerFragment.kt
@@ -9,12 +9,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.missclickads.cleaner.R
+import com.missclickads.cleaner.core.BaseFragment
 import com.missclickads.cleaner.databinding.FragmentBatteryOptimizerBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class BatteryOptimizerFragment : Fragment() {
+class BatteryOptimizerFragment : BaseFragment<BatteryOptimizerViewModel>() {
 
-    private val viewModel : BatteryOptimizerViewModel by viewModel()
+    override val viewModel : BatteryOptimizerViewModel by viewModel()
     private var _binding: FragmentBatteryOptimizerBinding? = null
 
     private val binding get() = _binding!!
@@ -28,9 +29,33 @@ class BatteryOptimizerFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUi()
+    }
+
+    private fun initUi(){
+        //todo buttons, etc
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun notOptimized() {
+        TODO("Not yet implemented")
+    }
+
+    override fun optimization() {
+        TODO("Not yet implemented")
+    }
+
+    override fun optimized() {
+        TODO("Not yet implemented")
+    }
+
+    override fun error() {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/missclickads/cleaner/ui/batteryoptimizer/BatteryOptimizerViewModel.kt
+++ app/src/main/java/com/missclickads/cleaner/ui/batteryoptimizer/BatteryOptimizerViewModel.kt
@@ -3,8 +3,9 @@ package com.missclickads.cleaner.ui.batteryoptimizer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.missclickads.cleaner.core.BaseViewModel
 
-class BatteryOptimizerViewModel : ViewModel() {
+class BatteryOptimizerViewModel : BaseViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
