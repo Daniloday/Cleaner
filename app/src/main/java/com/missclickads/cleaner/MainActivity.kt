64220353fa0a5c2c01fc68c23b07247b9319aa7f
daniloday <danil.vladyka1@gmail.com>
2021-10-20 21:32:52 +0300

    splash

--- app/src/main/java/com/missclickads/cleaner/MainActivity.kt
+++ app/src/main/java/com/missclickads/cleaner/MainActivity.kt
@@ -7,8 +7,14 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.missclickads.cleaner.utils.OptimizeData
+import com.missclickads.cleaner.utils.OptimizeDataSaver
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dataSaver: OptimizeDataSaver
+    private lateinit var optimizeData : OptimizeData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +25,16 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         navView.itemIconTintList = null
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_phone_booster, R.id.navigation_cpu_cooler,  R.id.navigation_battery_optimizer,R.id.navigation_junk_cleaner,R.id.navigation_file_manager))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        dataSaver = OptimizeDataSaver(this)
+        optimizeData = dataSaver.getData()
+
     }
 
 }
---
+++ app/src/main/java/com/missclickads/cleaner/utils/OptimizeDataSaver.kt
@@ -0,0 +1,91 @@
+package com.missclickads.cleaner.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val S_PHONE_BOOSTER = "PHONE_BOOSTER"
+const val S_BATTERY_OPTIMIZER = "BATTERY_OPTIMIZER"
+const val S_CPU_COOLER = "CPU_COOLER "
+const val S_JUNK_CLEANER = "JUNK_CLEANER"
+
+const val S_PREF_NAME = "shpr"
+const val DATA_PATTERN = "dd-MM-yyyy HH:mm:ss"
+
+const val S_FIRST_LAUNCH = "FIRST_LAUNCH"
+const val BASE_VALUE = "First"
+
+class OptimizeDataSaver(context: Context) {
+
+    private var sPrefs : SharedPreferences = context.getSharedPreferences(S_PREF_NAME, Context.MODE_PRIVATE)
+    private val dateFormat: DateFormat = SimpleDateFormat(DATA_PATTERN)
+    private val formatter = SimpleDateFormat(DATA_PATTERN)
+
+    @SuppressLint("SimpleDateFormat")
+    fun saveOptimization(type: OptimizeType){
+        val date = Date()
+        when(type){
+            OptimizeType.PHONE_BOOSTER ->
+                sPrefs.edit().putString(S_PHONE_BOOSTER, dateFormat.format(date)).apply()
+            OptimizeType.BATTERY_OPTIMIZER ->
+                sPrefs.edit().putString(S_BATTERY_OPTIMIZER, dateFormat.format(date)).apply()
+            OptimizeType.CPU_COOLER ->
+                sPrefs.edit().putString(S_CPU_COOLER, dateFormat.format(date)).apply()
+            OptimizeType.JUNK_CLEANER ->
+                sPrefs.edit().putString(S_JUNK_CLEANER, dateFormat.format(date)).apply()
+        }
+    }
+
+    fun checkShowRate(): Boolean {
+        val date = Date()
+        val oneDay = 24 * 60 * 60 * 1000
+        val firstLaunch = sPrefs.getString(S_FIRST_LAUNCH, BASE_VALUE)
+        return if (firstLaunch == BASE_VALUE){
+            sPrefs.edit().putString(S_FIRST_LAUNCH, dateFormat.format(date)).apply()
+            false
+        } else {
+            date.time - formatter.parse(firstLaunch!!)!!.time > oneDay
+        }
+    }
+
+    fun getData():OptimizeData{
+        val timeUpdateStatus = 15 * 60 * 1000
+        val phoneBooster = getDataFormat(S_PHONE_BOOSTER)
+        val batteryOptimizer = getDataFormat(S_BATTERY_OPTIMIZER)
+        val cpuCooler = getDataFormat(S_CPU_COOLER)
+        val junkCleaner = getDataFormat(S_JUNK_CLEANER)
+        val date = Date()
+
+        return OptimizeData(
+            phoneBooster = date.time - phoneBooster.time <= timeUpdateStatus,
+            batteryOptimizer = date.time - batteryOptimizer.time <= timeUpdateStatus,
+            cpuCooler = date.time - cpuCooler.time <= timeUpdateStatus,
+            junkCleaner = date.time - junkCleaner.time <= timeUpdateStatus,
+        )
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getDataFormat(type : String):Date{
+        val dataStr = sPrefs.getString(type,"20-10-2020 12:12:12")
+        return formatter.parse(dataStr!!)!!
+    }
+
+
+}
+
+enum class OptimizeType{
+    PHONE_BOOSTER,
+    BATTERY_OPTIMIZER,
+    CPU_COOLER,
+    JUNK_CLEANER
+}
+
+data class OptimizeData(
+    val phoneBooster : Boolean,
+    val batteryOptimizer : Boolean,
+    val cpuCooler : Boolean,
+    val junkCleaner : Boolean,
+)
