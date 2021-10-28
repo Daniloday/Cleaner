5844cdfb4c038fe8eec6c52a83ef05f5efd83937
incarnadine24 <wenq.zz123@gmail.com>
2021-10-28 17:56:37 +0300

    fixed

--- app/src/main/java/com/missclickads/cleaner/ui/phonebooster/PhoneOptimizationDialogFragment.kt
+++ app/src/main/java/com/missclickads/cleaner/ui/phonebooster/PhoneOptimizationDialogFragment.kt
@@ -1,5 +1,6 @@
 package com.missclickads.cleaner.ui.phonebooster
 
+import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -9,8 +10,12 @@ import android.view.ViewGroup
 import android.view.Window
 import android.widget.LinearLayout
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.lifecycleScope
 import com.missclickads.cleaner.databinding.CompleteDialogFragmentBinding
 import com.missclickads.cleaner.databinding.PhoneBoosterOptimizationFragmentBinding
+import com.missclickads.cleaner.ui.optimazed.CompleteOptimizationDialogFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class PhoneOptimizationDialogFragment : DialogFragment() {
     private var _binding: PhoneBoosterOptimizationFragmentBinding? = null
@@ -31,6 +36,19 @@ class PhoneOptimizationDialogFragment : DialogFragment() {
             getDialog()?.getWindow()?.requestFeature(Window.FEATURE_NO_TITLE);
         }
         _binding = PhoneBoosterOptimizationFragmentBinding.inflate(inflater, container, false)
+        binding.apply {
+            val animation = ObjectAnimator.ofInt(progressBarCircle, "progress", 0, 100)
+            animation.duration = 5 * 1000
+            animation.start()
+            lifecycleScope.launch{
+                delay(5*1000)
+                val dialogCompleted = CompleteOptimizationDialogFragment()
+                dialogCompleted.show(childFragmentManager, "optimization")
+
+                dismiss()
+            }
+        }
+
         return binding.root
     }
 
