526cd7a6a8255e29aa0a8f8cde85cb38087016ef
incarnadine24 <wenq.zz123@gmail.com>
2021-10-31 17:37:02 +0200

    fixed

---
+++ app/src/main/java/com/missclickads/cleaner/ui/filemanager/FileManagerFilesListViewModelFragment.kt
@@ -0,0 +1,7 @@
+package com.missclickads.cleaner.ui.filemanager
+
+import androidx.lifecycle.ViewModel
+
+class FileManagerFilesListViewModelFragment : ViewModel() {
+    // TODO: Implement the ViewModel
+}
