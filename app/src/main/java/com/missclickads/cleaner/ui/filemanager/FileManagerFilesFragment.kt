1d254872047f8c3d6bb4557a218392d4f6942b90
Nikita <prilki.neket@gmail.com>
2021-10-31 18:47:35 +0300

    xml and files Fragment

--- app/src/main/java/com/missclickads/cleaner/ui/filemanager/FileManagerFilesFragment.kt
+++ app/src/main/java/com/missclickads/cleaner/ui/filemanager/FileManagerFilesFragment.kt
@@ -1,10 +1,37 @@
 package com.missclickads.cleaner.ui.filemanager
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.missclickads.cleaner.core.BaseFragment
+import com.missclickads.cleaner.databinding.FragmentFileManagerBinding
+import com.missclickads.cleaner.databinding.FragmentFileManagerFilesListFragmentBinding
 
 class FileManagerFilesFragment : Fragment() {
     val viewModel : FileManagerViewModel by viewModels()
+    private var _binding: FragmentFileManagerFilesListFragmentBinding? = null
 
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFileManagerFilesListFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUi()
+    }
+
+
+    private fun initUi(){
+
+    }
 }
--- app/src/main/java/com/missclickads/cleaner/ui/filemanager/FileManagerFilesListViewModelFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.missclickads.cleaner.ui.filemanager
-
-import androidx.lifecycle.ViewModel
-
-class FileManagerFilesListViewModelFragment : ViewModel() {
-    // TODO: Implement the ViewModel
-}
